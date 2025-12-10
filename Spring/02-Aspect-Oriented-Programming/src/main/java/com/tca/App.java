22d9894ba156aad0106b9583b8427da606d8f65d
Dnyaneshwar-Padule <dnyaneshwarpadule83@gmail.com>
2025-12-10 17:07:49 +0530

    Spring: Aspect oriented Programming

--- Spring/02-Aspect-Oriented-Programming/src/main/java/com/tca/App.java
+++ Spring/02-Aspect-Oriented-Programming/src/main/java/com/tca/App.java
@@ -1,13 +1,18 @@
 package com.tca;
 
+import org.springframework.aop.framework.ProxyFactoryBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.tca.dao.StudentDao;
+import com.tca.dao.StudentDaoImpl;
 import com.tca.entities.Student;
 import com.tca.service.StudentService;
+import com.tca.service.StudentServiceImpl;
+import com.tca.util.LoggerAdvice;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml"); 
     
        Student student = new Student();
@@ -16,5 +21,26 @@ public class App {
        
        StudentService studentService = (StudentService) context.getBean("studentService");
        studentService.addStudent(student);
+       
+       
+       // This happens Behind the scenes
+       
+       // first the StudentDao is created (because target needs it, DI)
+       StudentDao studentDao = new StudentDaoImpl();
+       
+       // Then target is created, with setting it's properties
+       StudentServiceImpl target = new StudentServiceImpl();
+       target.setStudentDao(studentDao);
+       
+       // Then ProxyFactoryBean is created
+       ProxyFactoryBean factory = new ProxyFactoryBean();
+       factory.setTarget(target);  // original object (setting target property)
+       factory.setProxyInterfaces(new Class[]{StudentService.class}); // setting proxy interfaces
+       //factory.setInterceptorNames(args);
+       factory.addAdvice(new LoggerAdvice());  // and wrapping another methods in proxy, like logger, security, transaction etc.. 
+    
+       StudentService s = (StudentService)factory.getObject();
+       s.addStudent(student);
+       
     }
 }
